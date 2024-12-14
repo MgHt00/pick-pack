6ff80893bc9b2466d299073954325d55500af47a
Mg Htoo <whshwe@gmail.com>
2024-12-14 09:59:55 +0630

    Mod started

---
+++ js/globals.js
@@ -0,0 +1,44 @@
+export default class Global {
+  constructor() {
+    this.bodyElement = document.body;
+    this.headerElement = document.querySelector("#frame-header");
+    this.frameLoadOrder = document.querySelector("#frame-load-order");
+    this.orderInput = document.querySelector("#order-input");
+    this.loadOrderBtn = document.querySelector("#load-order-btn");
+    this.resetBtn = document.querySelector("#reset-btn");
+    this.frameSKUContainer = document.querySelector("#frame-SKU-container");
+    this.frameScanBarcode = document.querySelector("#frame-scan-barcode");
+    this.barcodeInputTop = document.querySelector("#barcode-input-top");
+    this.barcodeLabel = document.querySelector("#barcode-label");
+    this.barcodeInput = document.querySelector("#barcode-input");
+    this.checkBarcodeBtn = document.querySelector("#check-barcode-btn");
+    this.frameOrderMessage = document.querySelector("#frame-order-message");
+    this.orderMessage = document.querySelector("#order-message");
+    this.frameProgressContainer = document.querySelector("#frame-progress-container");
+    this.successMessage = "All SKUs matched with barcodes successfully.";
+  }
+
+  readValue() {
+    
+  }
+
+  removeClass(targetElement, className) {
+    targetElement.classList.remove(className);
+    return this;
+  }
+
+  addClass(targetElement, className) {
+    targetElement.classList.add(className);
+    return this;
+  }
+
+  insertInnerHTML(targetElement, content) {
+    targetElement.innerHtml = content;
+    return this;
+  }
+
+  insertTextContent(targetElement, content) {
+    targetElement.textContent = content;
+    return this;
+  }
+}
--- js/pack.js
+++ js/pack.js
@@ -1,62 +1,56 @@
 import { consumerKey, consumerSecret } from './config.js';
+import Global from './globals.js';
 
 /*const consumerKey = '%%CONSUMER_KEY%%';
 const consumerSecret = '%%CONSUMER_SECRET%%';*/
 
+const globalInstance = new Global();
+
+(function initialize(){
+  globalInstance.checkBarcodeBtn.addEventListener("click", checkBarcode);
+  globalInstance.loadOrderBtn.addEventListener("click", loadOrder);
+  globalInstance.resetBtn.addEventListener("click", resetAll);
+
+  // EVENT: Listeners
+  // To ensure that the DOM is fully loaded before the script executes
+  document.addEventListener("DOMContentLoaded", function() {
+    globalInstance.orderInput.focus(); // focus on the input at start.
+    resetAll(); // Reset everything at the start.
+  });
+
+  globalInstance.orderInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      loadOrder();
+    }
+  });
+
+  globalInstance.barcodeInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      checkBarcode();
+    }
+  });
+})();
+
 let orderItems = []; // to fetch all info of an order
 let orderedSKUs = []; // to keep the ordered SKUs
-const bodyElement = document.body;
-const headerElement = document.querySelector("#frame-header");
-const frameLoadOrder = document.querySelector("#frame-load-order");
-const orderInput = document.querySelector("#order-input");
-const loadOrderBtn = document.querySelector("#load-order-btn");
-const resetBtn = document.querySelector("#reset-btn");
-const frameSKUContainer = document.querySelector("#frame-SKU-container");
-const frameScanBarcode = document.querySelector("#frame-scan-barcode");
-const barcodeInputTop = document.querySelector("#barcode-input-top");
-const barcodeLabel = document.querySelector("#barcode-label");
-const barcodeInput = document.querySelector("#barcode-input");
-const checkBarcodeBtn = document.querySelector("#check-barcode-btn");
-const frameOrderMessage = document.querySelector("#frame-order-message");
-const orderMessage = document.querySelector("#order-message");
-const frameProgressContainer = document.querySelector("#frame-progress-container");
-const successMessage = "All SKUs matched with barcodes successfully.";
 let orderID = 0;
 let totalSKUs = 0;
 
-// EVENT: Listeners
-// To ensure that the DOM is fully loaded before the script executes
-document.addEventListener("DOMContentLoaded", function() {
-  orderInput.focus(); // focus on the input at start.
-  resetAll(); // Reset everything at the start.
-});
-
-checkBarcodeBtn.addEventListener("click", checkBarcode);
-loadOrderBtn.addEventListener("click", loadOrder);
-resetBtn.addEventListener("click", resetAll);
-
-orderInput.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    loadOrder();
-  }
-});
-
-barcodeInput.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    checkBarcode();
-  }
-});
-
 // FUNCTION: To load an order with a user input
 async function loadOrder() {
-  frameOrderMessage.classList.remove("hidden");
-  orderMessage.classList.remove("success-message");
-  orderID = orderInput.value; // Read the order ID before resetting
+  /*globalInstance.frameOrderMessage.classList.remove("hidden");
+  globalInstance.orderMessage.classList.remove("success-message");*/
+  globalInstance
+    .removeClass(globalInstance.frameOrderMessage, "hidden")
+    .removeClass(globalInstance.orderMessage, "success-message");
+
+  orderID = globalInstance.orderInput.value; // Read the order ID before resetting
+
   if (!orderID) {
     playWrongSound();
-    orderMessage.textContent = "Enter an order ID to load.";
+    globalInstance.orderMessage.textContent = "Enter an order ID to load.";
     //playBeepSound();
-    orderInput.focus();
+    globalInstance.orderInput.focus();
     return;
   }
 
@@ -65,18 +59,24 @@ async function loadOrder() {
   // Check if the order has already been checked
   // Need to ensure that the checkOrderNote function is called and 
   // awaited properly within the loadOrder function.
-  frameOrderMessage.classList.remove("hidden");
-  const isOrderChecked = await checkOrderNote(orderID, successMessage);
+
+  /*globalInstance.frameOrderMessage.classList.remove("hidden");*/
+  globalInstance.removeClass(globalInstance.frameOrderMessage, "hidden");
+
+  const isOrderChecked = await checkOrderNote(orderID, globalInstance.successMessage);
   if (isOrderChecked) {
-    orderMessage.innerHTML = "Order already checked.<br>Enter another order.";
-    orderMessage.classList.add("success-message");
-    orderInput.value = "";
-    orderInput.focus();
+    /*globalInstance.orderMessage.innerHTML = "Order already checked.<br>Enter another order.";
+    globalInstance.orderMessage.classList.add("success-message");*/
+    globalInstance.insertInnerHTML(globalInstance.orderMessage, "Order already checked.<br>Enter another order.");
+    globalInstance.addClass(globalInstance.orderMessage, "success-message");
+    globalInstance.orderInput.value = "";
+    globalInstance.orderInput.focus();
     playWrongSound();
     return;
   }
 
-  orderMessage.textContent = "Order loading...";
+  /*globalInstance.orderMessage.textContent = "Order loading...";*/
+  globalInstance.insertTextContent(globalInstance.orderMessage, "Order loading...");
   await fetchOrderItems(orderID); // ADD await
 }
 
@@ -116,7 +116,7 @@ async function fetchOrderItems(orderId) {
     const quantity = Number(orderItem["quantity"]);
     /*console.log(`Order quantity ${quantity}`);*/
 
-    frameSKUContainer.classList.remove("hidden");
+    globalInstance.frameSKUContainer.classList.remove("hidden");
 
     // Add ordered SKU to the `orderedSKUs` array, and increment the counter.
     for (let i = 0; i < quantity; i++) {
@@ -125,46 +125,46 @@ async function fetchOrderItems(orderId) {
       const sku = document.createElement("p");
       sku.setAttribute("id", `${orderItem["sku"]}`);
       sku.textContent = orderItem["sku"];
-      frameSKUContainer.append(sku);
+      globalInstance.frameSKUContainer.append(sku);
     }   
   }
 
-  bodyElement.classList.remove("start");
-  bodyElement.classList.add("transition");
+  globalInstance.bodyElement.classList.remove("start");
+  globalInstance.bodyElement.classList.add("transition");
 
-  headerElement.classList.add("hidden");
+  globalInstance.headerElement.classList.add("hidden");
 
-  orderMessage.textContent = `${orderId} Loaded.`;
-  orderMessage.classList.add("loaded");
+  globalInstance.orderMessage.textContent = `${orderId} Loaded.`;
+  globalInstance.orderMessage.classList.add("loaded");
   playBeepSound();
 
-  frameLoadOrder.classList.add("hidden");
-  frameScanBarcode.classList.remove("hidden");
+  globalInstance.frameLoadOrder.classList.add("hidden");
+  globalInstance.frameScanBarcode.classList.remove("hidden");
 
-  orderMessage.classList.add("transition");
-  resetBtn.classList.remove("hidden");
+  globalInstance.orderMessage.classList.add("transition");
+  globalInstance.resetBtn.classList.remove("hidden");
 
-  frameScanBarcode.classList.add("transition");
+  globalInstance.frameScanBarcode.classList.add("transition");
 
   enableBarcode();
-  barcodeInput.focus();
+  globalInstance.barcodeInput.focus();
   
 } catch (error) {
     console.error('Error fetching order data:', error);
 
     // Handle specific error messages
     if (error.message === "Request timed out") {
-      orderMessage.innerHTML = "Order loading timed out. Please try again.";
+      globalInstance.orderMessage.innerHTML = "Order loading timed out. Please try again.";
     } else if (error.response && error.response.status === 404) {
-      orderMessage.innerHTML = "Order not found!";
+      globalInstance.orderMessage.innerHTML = "Order not found!";
     } else {
-      orderMessage.innerHTML = "Error loading order.<br>Please try again.";
+      globalInstance.orderMessage.innerHTML = "Error loading order.<br>Please try again.";
     }
 
-    orderMessage.classList.add("error-message");
+    globalInstance.orderMessage.classList.add("error-message");
     playWrongSound();
-    orderInput.value = "";
-    orderInput.focus();
+    globalInstance.orderInput.value = "";
+    globalInstance.orderInput.focus();
     return; // Exit the function if there is an error
   }
 } 
@@ -172,9 +172,9 @@ async function fetchOrderItems(orderId) {
 
 // FUNCTION: To match scanned-SKUs with ordered-SKUs
 async function checkBarcode() {
-  frameProgressContainer.classList.remove("hidden");
+  globalInstance.frameProgressContainer.classList.remove("hidden");
 
-  const barcode = barcodeInput.value;
+  const barcode = globalInstance.barcodeInput.value;
   const checkedSKUparagraph = document.createElement("p");
   const existingError = document.querySelector("#barcodeError");
 
@@ -187,13 +187,13 @@ async function checkBarcode() {
 
   // Display error if barcode input is empty
   if (!barcode) {
-    frameProgressContainer.innerHTML = "";
+    globalInstance.frameProgressContainer.innerHTML = "";
     const errorParagraph = document.createElement("p");
     errorParagraph.textContent = "Scan a barcode to check.";
     errorParagraph.classList.add("error-message");
     errorParagraph.setAttribute("id", "barcodeError");
     //errorParagraph.style.color = "red";
-    frameProgressContainer.append(errorParagraph);
+    globalInstance.frameProgressContainer.append(errorParagraph);
     playWrongSound();
     return;
   }
@@ -201,14 +201,14 @@ async function checkBarcode() {
   // Iterate through the ordered SKUs to find a match
   for (let i = 0; i < orderedSKUs.length; i++) {
     if (barcode === orderedSKUs[i]) {
-      ///orderMessage.textContent = "";
+      ///globalInstance.orderMessage.textContent = "";
       //checkedSKUparagraph.textContent = orderedSKUs[i];
       // If scanned barcode is same as orderedSKU, matched SKU is removed from the frame-SKU-container
       // and put it in the progress-container; loop until the end of orderedSKUs array.
       
-      frameProgressContainer.classList.remove("error-message");
-      frameProgressContainer.innerHTML = "Correct!! Scan another.";
-      frameProgressContainer.classList.add("success-message");
+      globalInstance.frameProgressContainer.classList.remove("error-message");
+      globalInstance.frameProgressContainer.innerHTML = "Correct!! Scan another.";
+      globalInstance.frameProgressContainer.classList.add("success-message");
       
       document.querySelector(`#${orderedSKUs[i]}`).classList.add("checked-sku");
 
@@ -219,23 +219,23 @@ async function checkBarcode() {
 
       skuFound = true;
       playBeepSound();
-      barcodeInput.value = "";
-      barcodeInput.focus();
+      globalInstance.barcodeInput.value = "";
+      globalInstance.barcodeInput.focus();
       break; // To exit the loop immediately after processing the matched SKU.
     }
   } 
 
   // If no matching SKU is found
   if (!skuFound) {
-    //orderMessage.textContent = "Wrong Product";
+    //globalInstance.orderMessage.textContent = "Wrong Product";
 
-    frameProgressContainer.classList.remove("success-message");
-    frameProgressContainer.innerHTML = "Wrong Product";
-    frameProgressContainer.classList.add("error-message");
+    globalInstance.frameProgressContainer.classList.remove("success-message");
+    globalInstance.frameProgressContainer.innerHTML = "Wrong Product";
+    globalInstance.frameProgressContainer.classList.add("error-message");
     
     playWrongSound();
-    barcodeInput.value = "";
-    barcodeInput.focus();
+    globalInstance.barcodeInput.value = "";
+    globalInstance.barcodeInput.focus();
   }
 
   // Check if all SKUs are scanned
@@ -244,18 +244,18 @@ async function checkBarcode() {
     //playCorrectSound();
     playCompleteSound();
 
-    orderMessage.textContent = "Order complete!";
-    orderMessage.classList.add("order-complete");
+    globalInstance.orderMessage.textContent = "Order complete!";
+    globalInstance.orderMessage.classList.add("order-complete");
 
-    frameSKUContainer.classList.add("hidden");
-    frameProgressContainer.classList.add("hidden");
-    frameScanBarcode.classList.add("hidden");
+    globalInstance.frameSKUContainer.classList.add("hidden");
+    globalInstance.frameProgressContainer.classList.add("hidden");
+    globalInstance.frameScanBarcode.classList.add("hidden");
 
-    resetBtn.textContent = "Check a new order";
+    globalInstance.resetBtn.textContent = "Check a new order";
     const orderId = orderID; 
 
     /* TEMP COMMENT */
-    await appendOrderNoteAndChangeStatus(orderId, successMessage);
+    await appendOrderNoteAndChangeStatus(orderId, globalInstance.successMessage);
     
     console.log("appendOrderNoteAndChangeStatus() is called");
   }
@@ -341,7 +341,7 @@ async function appendOrderNoteAndChangeStatus(orderId, successMessage) {
 
 // FUNCTION:
 async function checkOrderNote(orderId, successMessage) {
-  orderMessage.textContent = "Order loading...";
+  globalInstance.orderMessage.textContent = "Order loading...";
 
   const auth = btoa(`${consumerKey}:${consumerSecret}`);
   const noteURL = `https://mmls.biz/wp-json/wc/v3/orders/${orderId}/notes`;
@@ -378,57 +378,57 @@ async function checkOrderNote(orderId, successMessage) {
 // MISC functions
 // FUNCTION: To disable barcode input and button
 function disableBarcode() {
-  barcodeInputTop.classList.add("disabled");
-  barcodeLabel.classList.add("disabled");
-  barcodeInput.disabled = true;
-  checkBarcodeBtn.disabled = true;
+  globalInstance.barcodeInputTop.classList.add("disabled");
+  globalInstance.barcodeLabel.classList.add("disabled");
+  globalInstance.barcodeInput.disabled = true;
+  globalInstance.checkBarcodeBtn.disabled = true;
 }
 
 // FUNCTION: To enable barcode input and button
 function enableBarcode() {
-  barcodeInputTop.classList.remove("disabled");
-  barcodeLabel.classList.remove("disabled");
-  barcodeInput.disabled = false;
-  checkBarcodeBtn.disabled = false;
+  globalInstance.barcodeInputTop.classList.remove("disabled");
+  globalInstance.barcodeLabel.classList.remove("disabled");
+  globalInstance.barcodeInput.disabled = false;
+  globalInstance.checkBarcodeBtn.disabled = false;
 }
 
 // FUNCTION: To reset when Load Order is pressed.
 function resetAll() {
-  bodyElement.classList.add("start");
+  globalInstance.bodyElement.classList.add("start");
 
-  headerElement.className = "";
-  headerElement.classList.remove("hidden");
+  globalInstance.headerElement.className = "";
+  globalInstance.headerElement.classList.remove("hidden");
 
-  frameLoadOrder.className = "";
-  frameLoadOrder.classList.remove("hidden");
+  globalInstance.frameLoadOrder.className = "";
+  globalInstance.frameLoadOrder.classList.remove("hidden");
 
-  frameOrderMessage.className = "";
-  frameOrderMessage.classList.add("hidden");
+  globalInstance.frameOrderMessage.className = "";
+  globalInstance.frameOrderMessage.classList.add("hidden");
 
-  orderMessage.textContent = "";
-  orderMessage.className = "";
-  resetBtn.textContent =  "Reset";
-  resetBtn.classList.add("hidden");
+  globalInstance.orderMessage.textContent = "";
+  globalInstance.orderMessage.className = "";
+  globalInstance.resetBtn.textContent =  "Reset";
+  globalInstance.resetBtn.classList.add("hidden");
 
-  frameSKUContainer.className = "";
-  frameSKUContainer.classList.add("hidden");
-  frameSKUContainer.innerHTML = "";
+  globalInstance.frameSKUContainer.className = "";
+  globalInstance.frameSKUContainer.classList.add("hidden");
+  globalInstance.frameSKUContainer.innerHTML = "";
 
-  frameProgressContainer.className = "";
-  frameProgressContainer.classList.add("hidden");
-  frameProgressContainer.innerHTML = "";
+  globalInstance.frameProgressContainer.className = "";
+  globalInstance.frameProgressContainer.classList.add("hidden");
+  globalInstance.frameProgressContainer.innerHTML = "";
 
-  frameScanBarcode.className = "";
-  frameScanBarcode.classList.add("hidden");
+  globalInstance.frameScanBarcode.className = "";
+  globalInstance.frameScanBarcode.classList.add("hidden");
   
   disableBarcode();
-  //orderMessage.textContent = "Ready to begin.";
+  //globalInstance.orderMessage.textContent = "Ready to begin.";
   orderItems = []; 
   orderedSKUs = [];
   totalSKUs = 0; 
   
-  orderInput.value = "";
-  orderInput.focus();
+  globalInstance.orderInput.value = "";
+  globalInstance.orderInput.focus();
 }
 
 // Sounds
